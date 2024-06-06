81efc299d9c90335142b8ae4559d8bc46223d9c8
HarshithMaramraju <120772310+HarshithMaramraju@users.noreply.github.com>
2024-06-06 10:01:57 +0530

    Add files via upload

--- MailMessanger/MailMessanger.java
+++ MailMessanger/MailMessanger.java
@@ -1,1 +1,27 @@
+package BankingApplicationSpringBoot.MailMessanger;
 
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import BankingApplicationSpringBoot.Config.Mail;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+public class MailMessanger {
+
+	public static void htmlEmailMessanger(String from, String toMail, String subject, String body) throws MessagingException {
+//		mail config
+		JavaMailSender sender = Mail.getMailConfig();
+//		ste mime message
+		MimeMessage message = sender.createMimeMessage();
+//		set mime message helper
+		MimeMessageHelper htmlMessage = new MimeMessageHelper(message,true);
+//		set mail atribute/properties
+		htmlMessage.setTo(toMail);
+		htmlMessage.setFrom(from);
+		htmlMessage.setSubject(subject);
+		htmlMessage.setText(body, true);
+//		send message
+		sender.send(message);
+	}
+}
