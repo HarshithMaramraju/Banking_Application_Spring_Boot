6d174169f4898a90b4db054507ba7f1cfdf40c04
HarshithMaramraju <120772310+HarshithMaramraju@users.noreply.github.com>
2024-06-06 09:58:53 +0530

    Add files via upload

--- ControllerAdvicer/AdvicorController.java
+++ ControllerAdvicer/AdvicorController.java
@@ -1,1 +1,15 @@
+package BankingApplicationSpringBoot.ControllerAdvicer;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import BankingApplicationSpringBoot.Model.User;
+
+@ControllerAdvice
+public class AdvicorController {
+	
+	@ModelAttribute("registerUser")
+	public User getUserDefaults() {
+		return new User();
+	}
+}
