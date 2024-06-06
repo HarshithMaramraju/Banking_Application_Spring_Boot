7d4fe516852d75c6255da76002fb9f6bab0c6c49
HarshithMaramraju <120772310+HarshithMaramraju@users.noreply.github.com>
2024-06-06 10:04:49 +0530

    Add files via upload

---
+++ BankingApplicationSpringBootApplication.java
@@ -0,0 +1,13 @@
+package BankingApplicationSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingApplicationSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingApplicationSpringBootApplication.class, args);
+	}
+}
+
